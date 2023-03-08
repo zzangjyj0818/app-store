a366e8ca2a83dfc2a8b4446addba604c1c0607a4
Jeong Yeon Jae <jyj010818@naver.com>
2023-03-09 08:58:00 +0900

    connect API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import clayful from "clayful/client-js";
+import axios from 'axios';
+
+//API Token
+clayful.config({
+  client : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImNhMmJiMjYzMzM3NTYwY2YyZmU2YWQ1Zjg1NmYxYWU1OGJiZDNhMWQ3NTViZWFjODFhNzBkNTgyZjM4NzdhMTkiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjc3MDc1Nzg5LCJzdG9yZSI6IkdKWkRSNktYOEVVUi5XOTJOTjJUQUJZWVoiLCJzdWIiOiJaVzhZTldQWU1FTUIifQ.RCXLJzmpKt7DRhUuyvztcgmAxCjd36rUl4_iOeOLhHc'
+});
+
+clayful.install('request', require('clayful/plugins/request-axios')(axios));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import clayful from 'clayful/client-js';
 
 const LandingPage = () => {
+    var Product = clayful.Product;
+
+    var options = {
+        query: {
+            page : 1
+        }
+    };
+
+    Product.list(options, function(err,response){
+        if(err){
+            console.log(err.isClayful);
+            console.log(err.model);
+            console.log(err.method);
+            console.lof(err.status);
+            console.log(err.headers);
+            console.log(err.code);
+            console.log(err.message);
+        }
+        console.log(response.status);
+        console.log(response.headers);
+        console.log(response.data);
+    });
     return (
+
         <div>
-            
+            LandingPage
         </div>
     );
 };
